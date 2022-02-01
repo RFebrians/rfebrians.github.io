a9f52691c942aefd2d4d650cff3e84dcfb7fe079
R. Febriansyah <71978432+RFebrians@users.noreply.github.com>
2022-02-01 13:45:12 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ module.exports = {
     siteTitleAlt: `RFebrians - Portfolio`,
     siteHeadline: `RFebrians - Portfolio`,
     siteUrl: `https://rfebrians.github.io`,
-    siteDescription: `A Portfolio is a single page `,
+    siteDescription: `A Portfolio with a single page `,
     siteLanguage: `en`,
-    siteImage: `/portfolio.jpg`,
+    siteImage: `./static/portfolio.jpg`,
     author: `@r`
   },
   plugins: [

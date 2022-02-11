a01c86f1d9c748da374b049c9bfd8b3a9ac4fec3
RFebrians <RFebrians@users.noreply.github.com>
2022-02-11 06:53:57 +0000

    deploy: dccb2f2184d6da729857b1ecb513b0c754efbf22

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-cf31e5a75b7dedd4387a.js"
+    "url": "webpack-runtime-a1eef78099b376925599.js"
   },
   {
     "url": "framework-951c6195947af4b814f1.js"
   },
   {
-    "url": "app-68a7cc24bb759c0b7472.js"
+    "url": "app-6393394ef7229c59f03d.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b6e701e671d7dbd76073c058334ba37d"
+    "revision": "8b28834c7f847c11381220271ad886e2"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-189a951ec5956855d5fd.js"
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-68a7cc24bb759c0b7472.js`))) {
+  if (!resources || !(await caches.match(`/app-6393394ef7229c59f03d.js`))) {
     return await fetch(event.request)
   }
 

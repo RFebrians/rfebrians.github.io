a7b1fd6cb5a75aebdb75e7f78b8a36d2ac5d4720
RFebrians <ekikz1997@gmail.com>
2022-02-28 16:50:11 +0000

    added carousel

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -17,24 +17,22 @@ import Hero from '../@lekoarts/gatsby-theme-cara/components/hero'
 const GalleryPage = () => (
   <Layout>
     <Seo title="Gallery Page " />
-    <Parallax pages={2}>
-      <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={0} factor={1}>
         <Inner>
+          <Themed.h3>Judul</Themed.h3>
           <div>
-            <Themed.h1>Welcome to BlogSite</Themed.h1>
-            <Themed.h4>This is a Blog Section </Themed.h4>
-
             <Carousel autoPlay>
               <div>
-                <img alt="" src="https://i.imgur.com/MZljwLL.jpeg" />
+                <Themed.h1>Test Gambar Judul Mountain</Themed.h1>
+                
+                <img alt="" src="https://images.unsplash.com/photo-1465056836041-7f43ac27dcb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" />
                 <p className="legend">Legend 1</p>
               </div>
               <div>
-                <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
+                <img alt="" src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
                 <p className="legend">Legend 2</p>
               </div>
               <div>
-                <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" />
+                <img alt="" src="https://images.unsplash.com/photo-1498855926480-d98e83099315?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
                 <p className="legend">Legend 3</p>
               </div>
               <div>
@@ -83,9 +81,8 @@ const GalleryPage = () => (
               </div>
             </Carousel>
           </div>
+          <Themed.h1>sjdfkslfj</Themed.h1>
         </Inner>
-      </Content>
-    </Parallax>
   </Layout>
 )
 

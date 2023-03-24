02c294ee070a3492ad041b5744cf7e90c79a9313
Zegveld <ekikz1997@gmail.com>
2023-03-24 15:53:48 +0700

    Test CI/CD

--- src/@lekoarts/gatsby-theme-cara/components/hero.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/hero.tsx
@@ -142,7 +142,7 @@ const Hero: React.FC<{ offset: number; factor?: number }> = ({ offset, factor = 
               borderRadius: 5
             }}
           >
-            Welcome ! Test
+            Welcome ! Test CI/CD
           </h1>
           <h2
             sx={{
